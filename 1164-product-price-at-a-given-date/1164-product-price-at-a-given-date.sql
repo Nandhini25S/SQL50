0e1885aa99c9aba779483345ec70353412de5021
Nandhini Sivakumar <nandhinivadalur@gmail.com>
2023-11-09 13:21:00 +0530

    Time: 792 ms (78.10%), Space: 0B (100.00%) - LeetHub

---
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+
+SELECT product_id, IFNULL(price,10) AS price
+FROM
+ (SELECT distinct product_id FROM Products) AS uniqueProducts
+ LEFT JOIN
+ (SELECT distinct product_id, 
+   FIRST_VALUE(new_price) OVER (PARTITION BY product_id ORDER BY change_date DESC) AS price
+  FROM Products WHERE change_date<='2019-08-16') AS final_price USING (product_id);
