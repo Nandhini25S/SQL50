990e92fa1e92224f2e0dec6dcb6bbeb04fba25c3
Nandhini Sivakumar <nandhinivadalur@gmail.com>
2023-11-09 13:24:41 +0530

    Time: 980 ms (37.50%), Space: 0B (100.00%) - LeetHub

--- 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -7,3 +7,4 @@ FROM
  (SELECT distinct product_id, 
    FIRST_VALUE(new_price) OVER (PARTITION BY product_id ORDER BY change_date DESC) AS price
   FROM Products WHERE change_date<='2019-08-16') AS final_price USING (product_id);
+# ON uniqueProducts.product_id = final_price.product_id
